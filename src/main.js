9d080cb58b46707eedabb5b301a1d6fe6dc3e8b4
Aloe <162494218+aceynk@users.noreply.github.com>
2024-06-15 19:42:45 -0700

    Update main.js
    maybe this'll be more consistent?

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
 
 async function fetch_list() {
-    const request_url = "https://cdn.jsdelivr.net/gh/aceynk/charlotteworld@master/users.json";
+    const request_url = "https://cdn.jsdelivr.net/gh/aceynk/charlotteworld@main/users.json";
     const request = new Request(request_url);
     const response = await fetch(request);
     const userlist = response.json();
